538a5586416924f43ae3987178d48c1a76fb6af4
khaled399 <111515359+khaled399@users.noreply.github.com>
2024-08-22 16:10:03 +0300

    Add files via upload

---
+++ java asigment/Task.js
@@ -0,0 +1,36 @@
+// let x = 10;
+
+// function khaled (){
+//     let x = 20 ;
+//     console.log(x)
+// }
+// khaled() // output => 20 local Scope
+
+/////////////////////////////////////
+
+// let y = 10;
+
+// function khaled (){
+//     console.log(y)
+// }
+// khaled() // 10 Global Scope
+
+// /////////////////
+
+// var z = 17;
+
+// function kk (){
+//     var z = 50 ;
+//     console.log(z)
+// }
+// kk() // 50 Local Group
+
+/////////////////////
+
+// let a = 15;
+
+// function k1 (){
+
+//     console.log(a)
+// }
+// boda() // 15 Global Group
